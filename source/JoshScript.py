b4633bffc8986838777a9025992b0a95cd76bc6b
Josh <30339741+JoshSCF@users.noreply.github.com>
2017-08-23 17:31:19 +0100

    Create JoshScript.py

---
+++ source/JoshScript.py
@@ -0,0 +1,46 @@
+# JoshScript Source *.jsh
+from sys import argv
+from re import findall
+value = 0
+
+def check(cd):
+    for i in findall("....?", cd):
+        if i.lower() != "josh":
+            return False
+    return True
+
+def error(msg):
+    print("JoshScript Error // " + msg)
+
+def interpret(code):
+    if len(code) % 4 == 0 and check(code):
+        for i in findall("....?", code):
+            global value
+            if i == "JOSH": value += 1
+            elif i == "josh": value -= [0, 1][value > 0]
+            elif i == "Josh": value *= 2
+            elif i == "josH": value = int(value / 2)
+            elif i == "JOsh": print(end = chr(value))
+            elif i == "joSH":
+                try:
+                    value = ord(input("> "))
+                except:
+                    error("Input is not a valid character")
+            else:
+                error("Not a valid josh")
+    else:
+        error("Code is not 100% josh, ensure there is no unintended spacing.")
+
+def openfile(filename):
+    data = open(filename, "r").read()
+    interpret(data)
+
+def runall():
+    try:
+        openfile(argv[1])
+    except:
+        error("Unknown error :(")
+
+print(end = "JoshScript v1.00\n> ")
+runall()
+print()
