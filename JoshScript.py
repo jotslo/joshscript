e3418b4c0a0c98b6ed1b82cd453a77c9d355557c
Josh <30339741+JoshSCF@users.noreply.github.com>
2017-08-25 20:55:04 +0100

    Update JoshScript.py

--- JoshScript.py
+++ JoshScript.py
@@ -1,7 +1,9 @@
 # JoshScript Source *.jsh
 from sys import argv
-from re import findall
-value = 0
+from re import *
+values = [0]*64
+pointer = 0
+ignored = [" ", "\t", "\n", chr(13), ";", "(", ")"]
 
 def check(cd):
     for i in findall("....?", cd):
@@ -12,17 +14,26 @@ def check(cd):
 def error(msg):
     print("JoshScript Error // " + msg)
 
+def removal(code):
+    for i in findall(compile(".*?\((.*?)\)"),code) + ignored:
+        code = code.replace(i, "")
+    return code
+
 def interpret(code):
+    code = removal(code)
     if len(code) % 4 == 0 and check(code):
         for i in findall("....?", code):
-            global value
-            if i == "JOSH": value += 1
-            elif i == "josh": value -= [0, 1][value > 0]
-            elif i == "Josh": value *= 2
-            elif i == "josH": value = int(value / 2)
-            elif i == "JOsh": print(end = chr(value))
-            elif i == "jOsh": value = 0
-            elif i == "JOSh": value **= 2
+            global values, pointer
+            if i == "JOSH": values[pointer] += [0, 1][values[pointer] < 256]
+            elif i == "josh": values[pointer] -= [0, 1][values[pointer] > 0]
+            elif i == "Josh": values[pointer] *= 2
+            elif i == "josH": values[pointer] = int(values[pointer] / 2)
+            elif i == "JOsh": print(end = chr(values[pointer]))
+            elif i == "jOsh": values[pointer] = 0
+            elif i == "JOSh": values[pointer] **= 2
+            elif i == "JosH": print(end = str(values[pointer]))
+            elif i == "JoSH": pointer = (pointer + 1) % 64
+            elif i == "joSh": pointer -= [1, -63][pointer < 1]
             elif i == "joSH":
                 try:
                     value = ord(input("\nIN> "))
@@ -38,11 +49,19 @@ def openfile(filename):
     interpret(data)
 
 def runall():
+    global values, pointer
     try:
         openfile(argv[1])
     except:
-        error("Unknown error :(")
+        try:
+            print("Something went wrong, loading interpreter...")
+            while True:
+                pointer = 0
+                values = [0]*64
+                interpret(input("\n> "))
+        except:
+            error("Unknown Error :(")
 
-print(end = "JoshScript v1.02\n> ")
+print(end = "JoshScript 1.1\n> ")
 runall()
 print()
