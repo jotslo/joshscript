2f8335f07f896024c657a38a8fde46b75f6e8aac
Edward J <user-e@users.noreply.github.com>
2017-12-21 20:26:08 +0000

    Use relative filename and a batch code comment.
    In one of the six forks of your repository I saw a relative filename used
    for the Python file - store it in the current folder with the batch code file.

--- association.bat
+++ association.bat
@@ -1,4 +1,7 @@
 @echo off
 title JoshScript 1.2
-py D:\Documents\Dev\JoshScript\source\JoshScript.py %*
+:: double-colon label = programming comment
+:: https://en.wikipedia.org/wiki/List_of_DOS_commands#REM
+:: Use relative filename (current folder) for Python file so it's easier.
+py JoshScript.py %*
 pause>nul
