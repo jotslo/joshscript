aef0ca1ceb0dfc0af644fb21b34ff503aa87e405
Edward J <user-e@users.noreply.github.com>
2018-07-28 18:55:44 +0100

    Update execute_script.bat

--- execute_script.bat
+++ execute_script.bat
@@ -2,7 +2,7 @@
 title JoshScript 1.2.3
 :: double-colon label = programming comment
 :: https://en.wikipedia.org/wiki/List_of_DOS_commands#REM
-:: Development file path shouldn't be used here, a relative
-:: file path (of Python file in current folder) is better.
+:: Development file path shouldn't be used here, the
+:: relative path of file in current folder is better.
 py JoshScript.py %*
 pause>nul
