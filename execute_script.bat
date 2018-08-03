6ee788b3706809c26b78b3c7168c67cc0e4d9b5a
Edward J <user-e@users.noreply.github.com>
2018-08-03 17:30:56 +0100

    Update execute_script.bat

--- execute_script.bat
+++ execute_script.bat
@@ -1,9 +1,9 @@
 @echo off
 title JoshScript 1.3.0
 :: double-colon = comment, https://en.wikipedia.org/wiki/List_of_DOS_commands#REM
-:: Development file path shouldn't be used here, the
-:: relative path of file in current folder is better.
+:: Development file path shouldn't be used here, the relative
+:: path of file in current folder is better. Or swap colons from
+:: the next line to one after in non-Github version of this file.
 :: py D:\Documents\Dev\JoshScript\source\JoshScript.py %*
-:: Or swap colons from previous line to next in dev file.
 py JoshScript.py %*
 pause>nul
